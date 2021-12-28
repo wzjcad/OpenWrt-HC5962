ae40466805c7d946cba6ef0ceb64002cfe4d3a48
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2021-12-28 19:15:30 +0800

    Update diy-part1.sh

--- hc5962/diy-part1.sh
+++ hc5962/diy-part1.sh
@@ -16,5 +16,7 @@
 # Add a feed source
 # echo 'src-git openwrt-vlmcsd https://github.com/cokebar/openwrt-vlmcsd.git' >>feeds.conf.default
 # echo 'src-git luci-app-vlmcsd https://github.com/cokebar/luci-app-vlmcsd.git' >>feeds.conf.default
-git clone https://github.com/cokebar/openwrt-vlmcsd.git package/vlmcsd
-git clone https://github.com/cokebar/luci-app-vlmcsd.git package/luci-app-vlmcsd
+# git clone https://github.com/cokebar/openwrt-vlmcsd.git package/vlmcsd
+# git clone https://github.com/cokebar/luci-app-vlmcsd.git package/luci-app-vlmcsd
+git clone https://github.com/siwind/openwrt-vlmcsd.git package/vlmcsd
+git clone https://github.com/siwind/luci-app-vlmcsd.git package/luci-app-vlmcsd
