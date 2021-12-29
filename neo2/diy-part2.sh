e9de825e0fff447c9622362beee13f992cd99b9c
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2021-12-29 21:26:36 +0800

    Update diy-part2.sh

--- neo2/diy-part2.sh
+++ neo2/diy-part2.sh
@@ -5,4 +5,4 @@ sed -i "s/'0.openwrt.pool.ntp.org'/'ntp.ntsc.ac.cn'/g" package/base-files/files/
 sed -i "s/'1.openwrt.pool.ntp.org'/'cn.ntp.org.cn'/g" package/base-files/files/bin/config_generate
 #
 sed -i 's/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
-sed -i "s/ucidef_set_interface_lan \"eth0\"/ucidef_set_interface_lan_wan \"eth0.1\" \"eth0\"/g" target/linux/sunxi/base-files/etc/board.d/02_network
+sed -i 's/static/dhcp/g' package/base-files/files/lib/functions/uci-defaults.sh
