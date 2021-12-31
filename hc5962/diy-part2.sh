3ba32063d5ccc19016a05526f2e40e9f464baef3
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2021-12-31 13:32:11 +0800

    Update diy-part2.sh

--- hc5962/diy-part2.sh
+++ hc5962/diy-part2.sh
@@ -2,9 +2,8 @@
 #
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 sed -i "s/'0.openwrt.pool.ntp.org'/'ntp.ntsc.ac.cn'/g" package/base-files/files/bin/config_generate
-sed -i "s/'1.openwrt.pool.ntp.org'/'cn.ntp.org.cn'/g" package/base-files/files/bin/config_generate
-sed -i "s/set system.@system[-1].timezone='UTC'/set system.@system[-1].timezone='CST-8'/g" package/base-files/files/bin/config_generate
-sed -i "/set system.@system[-1].timezone/i\		set system.@system[-1].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
+sed -i "s/set system.@system\[-1\].timezone='UTC'/set system.@system\[-1\].timezone='CST-8'/g" package/base-files/files/bin/config_generate
+sed -i "/set system.@system\[-1\].timezone/i\		set system.@system\[-1\].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
 #
 #sed -i 's/htmode=HT20/htmode=HT40/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
