54008a4bd95d209995c5e0165895a20ab46807bb
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2021-12-31 13:26:23 +0800

    Update diy-part2.sh

--- neo2/diy-part2.sh
+++ neo2/diy-part2.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
-sed -i "s/set system.@system[-1].hostname='OpenWrt'/set system.@system[-1].hostname='NanoPI-NEO2'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='NanoPI-NEO2'/g" package/base-files/files/bin/config_generate
 sed -i "s/'0.openwrt.pool.ntp.org'/'ntp.ntsc.ac.cn'/g" package/base-files/files/bin/config_generate
-sed -i "s/'1.openwrt.pool.ntp.org'/'cn.ntp.org.cn'/g" package/base-files/files/bin/config_generate
 #
 sed -i 's/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
 sed -i 's/static/dhcp/g' package/base-files/files/lib/functions/uci-defaults.sh
