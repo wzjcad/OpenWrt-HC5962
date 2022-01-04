71b985d665a684bc8bedfe1de0db98ac7efca238
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-01-04 09:06:36 +0800

    Update diy-part2.sh

--- x64/diy-part2.sh
+++ x64/diy-part2.sh
@@ -5,3 +5,6 @@ sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999
 sed -i "s/'0.openwrt.pool.ntp.org'/'ntp.ntsc.ac.cn'/g" package/base-files/files/bin/config_generate
 sed -i "s/set system.@system\[-1\].timezone='UTC'/set system.@system\[-1\].timezone='CST-8'/g" package/base-files/files/bin/config_generate
 sed -i "/set system.@system\[-1\].timezone/i\		set system.@system\[-1\].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
+#
+sed -i 's/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
+sed -i 's/static/dhcp/g' package/base-files/files/lib/functions/uci-defaults.sh
