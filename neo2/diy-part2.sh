5df6f7edf0b2f36d79215c691aa7731a0fdfe52a
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-01-04 16:36:47 +0800

    Update diy-part2.sh

--- neo2/diy-part2.sh
+++ neo2/diy-part2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
-sed -i "s/hostname='OpenWrt'/hostname='NanoPI-NEO2'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='NanoPi-NEO2'/g" package/base-files/files/bin/config_generate
 sed -i "s/'0.openwrt.pool.ntp.org'/'ntp.ntsc.ac.cn'/g" package/base-files/files/bin/config_generate
 sed -i "s/set system.@system\[-1\].timezone='UTC'/set system.@system\[-1\].timezone='CST-8'/g" package/base-files/files/bin/config_generate
 sed -i "/set system.@system\[-1\].timezone/i\		set system.@system\[-1\].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
 #
 sed -i 's/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
-sed -i 's/static/dhcp/g' package/base-files/files/lib/functions/uci-defaults.sh
+# sed -i 's/static/dhcp/g' package/base-files/files/lib/functions/uci-defaults.sh
+sed -i 's/ucidef_set_interface_lan \"eth0\"/ucidef_set_interface_wan \"eth0\"/g' target/linux/sunxi/base-files/etc/board.d/02_network
