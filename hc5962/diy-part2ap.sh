93ceb3d73cf63714a2bf7255a5067d1b295ce114
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-01-18 15:23:28 +0800

    Update diy-part2ap.sh

--- hc5962/diy-part2ap.sh
+++ hc5962/diy-part2ap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #用此脚本编译出来是AP模式，系统密码是password，无线密码是56781234
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
-sed -i "s/hostname='OpenWrt'/hostname='HiWifi'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='HiWifi_$(cat \/sys\/class\/ieee80211\/${dev}\/macaddress\|awk -F \":\" '{print $4\"\"$5\"\"$6 }'\| tr a-z A-Z)'/g" package/base-files/files/bin/config_generate
 sed -i "s/'0.openwrt.pool.ntp.org'/'ntp.ntsc.ac.cn'/g" package/base-files/files/bin/config_generate
 sed -i "s/set system.@system\[-1\].timezone='UTC'/set system.@system\[-1\].timezone='CST-8'/g" package/base-files/files/bin/config_generate
 sed -i "/set system.@system\[-1\].timezone/i\		set system.@system\[-1\].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
