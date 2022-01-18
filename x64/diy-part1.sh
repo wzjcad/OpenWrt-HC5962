883dc05e124cdcec532d802c8fca432c5607440a
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-01-18 13:49:47 +0800

    Update diy-part1.sh

--- x64/diy-part1.sh
+++ x64/diy-part1.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 #
-# Add a feed source
-# echo 'src-git openwrt-vlmcsd https://github.com/cokebar/openwrt-vlmcsd.git' >>feeds.conf.default
-# echo 'src-git luci-app-vlmcsd https://github.com/cokebar/luci-app-vlmcsd.git' >>feeds.conf.default
 git clone https://github.com/siwind/openwrt-vlmcsd.git package/vlmcsd
 git clone https://github.com/siwind/luci-app-vlmcsd.git package/luci-app-vlmcsd
-# git clone https://github.com/Wind4/vlmcsd.git package/vlmcsd
