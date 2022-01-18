a56ad4c2fb85b167b5c7e56509e1291ea4ace83a
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-01-18 13:52:04 +0800

    Update diy-part2.sh

--- hc5962/diy-part2.sh
+++ hc5962/diy-part2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+# sed -i "s/hostname='OpenWrt'/hostname='NanoPI-NEO2'/g" package/base-files/files/bin/config_generate
 sed -i "s/'0.openwrt.pool.ntp.org'/'ntp.ntsc.ac.cn'/g" package/base-files/files/bin/config_generate
 sed -i "s/set system.@system\[-1\].timezone='UTC'/set system.@system\[-1\].timezone='CST-8'/g" package/base-files/files/bin/config_generate
 sed -i "/set system.@system\[-1\].timezone/i\		set system.@system\[-1\].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
