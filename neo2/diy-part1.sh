cde66f4ee080901dac91b5439da481972f211e6e
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-03-15 21:59:38 +0800

    Update diy-part1.sh

--- neo2/diy-part1.sh
+++ neo2/diy-part1.sh
@@ -2,3 +2,4 @@
 #
 git clone https://github.com/siwind/openwrt-vlmcsd.git package/vlmcsd
 git clone https://github.com/siwind/luci-app-vlmcsd.git package/luci-app-vlmcsd
+git clone https://github.com/nixonli/ddns-scripts_dnspod.git package/ddns-scripts_dnspod
