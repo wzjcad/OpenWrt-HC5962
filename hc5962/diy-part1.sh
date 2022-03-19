b6eedab55383617a39145aa087165eee67d82cb1
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-03-19 15:24:42 +0800

    Update diy-part1.sh

--- hc5962/diy-part1.sh
+++ hc5962/diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 # echo 'src-git openwrt-vlmcsd https://github.com/cokebar/openwrt-vlmcsd.git' >>feeds.conf.default
 # echo 'src-git luci-app-vlmcsd https://github.com/cokebar/luci-app-vlmcsd.git' >>feeds.conf.default
+git clone https://github.com/nixonli/ddns-scripts_dnspod.git package/ddns-scripts_dnspod
