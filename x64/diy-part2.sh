d46b56fd9ef2d28430ec84c2d03b044d58345f3c
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-07-05 14:41:16 +0800

    Update diy-part2.sh

--- x64/diy-part2.sh
+++ x64/diy-part2.sh
@@ -8,5 +8,7 @@ sed -i "/set system.@system\[-1\].timezone/i\		set system.@system\[-1\].zonename
 sed -i "s/set network.globals.ula_prefix='auto'/set network.globals.ula_prefix='fd00::\/48'/g" package/base-files/files/bin/config_generate
 #
 sed -i 's/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
+sed -i 's/	option masq		1/	option masq		0/g' package/network/config/firewall/files/firewall.config
+sed -i 's/	option mtu_fix		1/	option mtu_fix		0/g' package/network/config/firewall/files/firewall.config
 # sed -i 's/static/dhcp/g' package/base-files/files/lib/functions/uci-defaults.sh
 sed -i 's/ucidef_set_interface \"lan\"/ucidef_set_interface \"wan\"/g' package/base-files/files/lib/functions/uci-defaults.sh
