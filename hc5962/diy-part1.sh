d504636e68881d363fc92d2f760a384ba9785cd2
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-10-31 20:05:15 +0800

    Update diy-part1.sh

--- hc5962/diy-part1.sh
+++ hc5962/diy-part1.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 # echo 'src-git openwrt-vlmcsd https://github.com/cokebar/openwrt-vlmcsd.git' >>feeds.conf.default
 # echo 'src-git luci-app-vlmcsd https://github.com/cokebar/luci-app-vlmcsd.git' >>feeds.conf.default
+# Add mtk driver
+rm -rf $GITHUB_WORKSPACE/openwrt/target/linux/ramips/*
+cp -r $GITHUB_WORKSPACE/immortalwrt/target/linux/ramips/. $GITHUB_WORKSPACE/openwrt/target/linux/ramips
